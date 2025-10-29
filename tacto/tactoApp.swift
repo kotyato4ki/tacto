bb8693f591b7dc4d292bab01b75feade31c5cb3f
nick <11andriianovnikita@gmail.com>
2025-10-29 19:41:44 +0300

    Initial Commit

---
+++ tacto/tactoApp.swift
@@ -0,0 +1,17 @@
+//
+//  tactoApp.swift
+//  tacto
+//
+//  Created by Nick on 29.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct tactoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
