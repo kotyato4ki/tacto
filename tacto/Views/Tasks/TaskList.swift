943ab6ed9d4e0b8f8013bb6b0b6a847c1f3ddc19
nick <11andriianovnikita@gmail.com>
2025-11-06 18:15:26 +0300

    ModifyTask

--- tacto/Views/Tasks/TaskList.swift
+++ tacto/Views/Tasks/TaskList.swift
@@ -11,16 +11,21 @@ struct TaskList: View {
     var tasks: [TaskModel]
     
     var body: some View {
-        List {
-            ForEach(tasks) { task in
-                TaskPreview(task: task)
-                    .frame(maxWidth: .infinity)
-                    .background(Color(NSColor.windowBackgroundColor))
-                    .clipShape(RoundedRectangle(cornerRadius: 6))
-                    .padding(.horizontal, 8)
+        NavigationStack {
+            List {
+                ForEach(tasks) { task in
+                    NavigationLink(destination: TaskView(task: task)) {
+                        TaskPreview(task: task)
+                            .frame(maxWidth: .infinity)
+                            .background(Color(NSColor.windowBackgroundColor))
+                            .clipShape(RoundedRectangle(cornerRadius: 6))
+                            .padding(.horizontal, 8)
+                    }
+                    .buttonStyle(.plain)
+                }
             }
+            .listStyle(.sidebar)
         }
-        .listStyle(.sidebar)
     }
 }
 
